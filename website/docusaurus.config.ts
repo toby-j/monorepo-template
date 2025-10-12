d1f8086841bfcf378d03a1bd56870bf06a358028
Toby Johnson <tobyojohnson@proton.me>
2025-10-12 21:03:05 +0100

    docusarus now has a base path

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -41,6 +41,7 @@ const config: Config = {
       {
         docs: {
           path: "../docs",
+          routeBasePath: "/",
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
